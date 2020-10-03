1f676531d53a24d39b1a3da965b6ea893464a9a5
u-Kotovsky <67106105+u-Kotovsky@users.noreply.github.com>
2020-10-03 23:49:46 +0400

    Update web.js

--- web.js
+++ web.js
@@ -1,6 +1,36 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-app.use(express.static(path.join(process.cwd(), 'source')));
-console.log(process.env.PORT || 5000);
-app.listen(process.env.PORT || 5000);
+console.clear()
+const log = console.log
+const path = require('path')
+const hapi = require('hapi')
+const inert = require('inert')
+// Маршруты могут быть сконфигурированы для объекта сервера
+const server = new hapi.Server({
+  port: 10101,
+  routes: {
+ files: {
+ relativeTo: path.join(__dirname, 'public')
+ }
+  }
+})
+const init = async () => {
+  // Команда server.register() добавляет плагин для приложения
+  await server.register(inert)
+  // inert добавляет обработчик папок, чтобы 
+  // указать маршрут для обслуживания нескольких файлов
+  server.route({
+ method: 'GET',
+ path: '/{param*}',
+ handler: {
+ directory: {
+ path: '.',
+ redirectToSlash: true,
+ index: true
+ }
+ }
+  })
+  await server.start()
+}
+init()
+log('\x1b[32m', '    -----------------------    ')
+log('\x1b[32m', '       Server is running       ')
+log('\x1b[32m', '    -----------------------    ')
