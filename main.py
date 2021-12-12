efd3e057a3419e4a3b1c97e15741958826734fb4
Vadimarere <74667626+Vadimarere@users.noreply.github.com>
2021-12-12 14:07:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,71 @@
-import random 
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
-key = "b66ef67b37817fff3ab83645022438af6b076103830a15d97d344168840f0361dcb1a4407cad0c1aa8621"
-# Авторизуемся как сообщество
-vk = vk_api.VkApi(token=key)
+import random
+import time
+import datetime
+token = "d5fa43392c8878ed152bed49d2d52ba01c54c9fdacc99ad7b1ea727864cac233ca10b00efc1310db92b45"
 
-def msg_001(user_id):
-    msg_send(user_id, 'text\ntext')
 
-def send_message(user_id, message):
-    from random import randint
-    vk.method('messages.send',
-        {'user_id': user_id,
-        "random_id":randint(1,1000) ,
-        'message': message,}
-    )
+vk = vk_api.VkApi(token=token)
 
-# Работа с сообщениями
-longpoll = VkLongPoll(vk)
-# Основной цикл
-for event in longpoll.listen():
-    # Если пришло новое сообщение
-    if event.type == VkEventType.MESSAGE_NEW:
-        # Если оно имеет метку для меня( то есть бота)
-        if event.to_me:
-            text = event.text.lower()
-            user_id = event.user_id
-            
-            get_msg = event.text.lower() # get_msg
-            msg_send = send_message # msg_send
+vk._auth_token()
 
-            if text == '=help': # 
-                send_message(user_id, 'Мои команды:')
+data = {}
+now = datetime.datetime.now()
+timeA = now.hour
+
+def construct(id,name,money,power):
+    p = {}
+    p["name"] = name
+    p["money"] = money
+    p["messegNumb"] = 0
+    p["power"] = power
+
+    data[id] = p
+    print(data)
+
+    return "normal"
+
+
+while True:
+    try:
+        now = datetime.datetime.now()
+        timeB = now.hour
+        
+        if timeA<timeB:
+            timeA = timeB
+
+            for i in data:
+                data[i]["money"] = data[i]["money"] + data[i]["power"]
+                print(data)
+        
+        elif (timeA>timeB) and (timeB == 0):
+            timeA = 0
+            for i in data:
+                data[i]["money"] = data[i]["money"] + data[i]["power"]
+
+
+        messages = vk.method("messages.getConversations", {"offset": 0, "count": 20, "filter": "unanswered"})
+        if messages["count"] >= 1:
+            id = messages["items"][0]["last_message"]["from_id"]
+            body = messages["items"][0]["last_message"]["text"]
+            if body.lower() == "привет":
                 
-            elif 'привет' in text: # привет > И тебе привет
-                msg_send(user_id, 'И тебе привет')
-                
-            elif 'шар' in get_msg or 'что' in get_msg:
-                msg_send(user_id, 'ЧТО')
-                
-            else: msg_send(user_id, 'Я не есть тебя понимать')
-            
-            print(event.user_id, ':', text)
-            
+                n = 0
+                for i in data:
+                    if id == i :
+                        n = 1
+                if n == 0:
+                    construct(id , id , 0 , 10)
+                print(data)
+
+
+                vk.method("messages.send", {"peer_id": id, "message": "Привет!", "random_id": random.randint(1, 2147483647)})
+            elif body.lower() == "кто я?":
+                vk.method("messages.send", {"peer_id": id, "message": "ты хороший человек", "random_id": random.randint(1, 2147483647)})
+            elif body.lower() == "стата":
+                smg = str(data[id]["money"])+"$"
+                smg = smg + "\n" + str(data[id]["power"]) + "доход/в час"
+                vk.method("messages.send", {"peer_id": id, "message": smg, "random_id": random.randint(1, 2147483647)})
+
+    except Exception as E:
+        time.sleep(1)
